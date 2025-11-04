b42b9125507155ad38d85b487671fc6fc0f9ce52
Niranjan Shanbhag <nshanbhag345@gmail.com>
2025-11-04 22:52:17 +0530

    Refactor import statement for PhotoDocumentation

--- src/pages/SkywalkAudit.tsx
+++ src/pages/SkywalkAudit.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Building, MapPin, Camera, FileText } from 'lucide-react';
-import PhotoDocumentation, { Photo } from 'src/components/PhotoDocumentation';
+import PhotoDocumentation from 'src/components/PhotoDocumentation';
 import { members } from '../data/members';
 
 const SkywalkAudit: React.FC = () => {
